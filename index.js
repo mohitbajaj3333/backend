8d90ef90adcb0b14f358212c2e6e460515a49ef7
Your Name <you@example.com>
2025-08-02 11:53:07 +0530

    s

--- index.js
+++ index.js
@@ -7,10 +7,7 @@ const PORT = process.env.PORT || 3000
 
 const app = express();
 
-app.use(cors({
-  origin: 'https://frontend-crnweb.vercel.app',
-  credentials: true,
-}));
+app.use(cors());
 app.use(express.json());
 
 
