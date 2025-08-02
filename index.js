21cb8a185fe5b0e57d2d15dda7515006b6169c22
Your Name <you@example.com>
2025-08-02 11:45:57 +0530

    cose edit

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ const axios = require('axios');
 const PORT = process.env.PORT || 3000
 
 const app = express();
-app.use(cors());
+
+app.use(cors({
+  origin: 'https://frontend-crnweb.vercel.app',
+  credentials: true,
+}));
 app.use(express.json());
 
 
